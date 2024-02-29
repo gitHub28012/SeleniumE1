355b27fa955233ea59f6cb9b7842cc1458029bdf
QSpiders <QSpiders@DESKTOP-U3SNJL3>
2024-02-29 20:37:34 +0530

    DataDriven Framework scripts

--- E1NewSeleniumProject/src/dataDrivenFramework/InvalidLoginTestCase.java
+++ E1NewSeleniumProject/src/dataDrivenFramework/InvalidLoginTestCase.java
@@ -8,22 +8,30 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class ValidLoginTestCase {
+public class InvalidLoginTestCase {
 
 	public static void main(String[] args) throws EncryptedDocumentException, IOException, InterruptedException {
-
+		
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 		driver.get("http://127.0.0.1/login.do");
 		
 		Flib flib = new Flib();
+		int rc = flib.getRowCount("./data/testData.xlsx","invalidcreds");
 		
-		driver.findElement(By.name("username")).sendKeys(flib.readExcelData("./data/testData.xlsx", "validcreds", 1, 0));
-		Thread.sleep(1000);
-		driver.findElement(By.name("pwd")).sendKeys(flib.readExcelData("./data/testData.xlsx", "validcreds", 1, 1));
-		Thread.sleep(1000);
-		driver.findElement(By.id("loginButton")).click();
+		for(int i=1; i <=rc; i++)
+		{
+			driver.findElement(By.name("username")).sendKeys(flib.readExcelData("./data/testData.xlsx", "invalidcreds", i, 0));
+			Thread.sleep(1000);
+			driver.findElement(By.name("pwd")).sendKeys(flib.readExcelData("./data/testData.xlsx", "invalidcreds", i, 1));
+	        Thread.sleep(1000);
+	        driver.findElement(By.id("loginButton")).click();
+	        
+	        Thread.sleep(1000);
+	        driver.findElement(By.name("username")).clear();
+		}
 
 	}
+
 }
