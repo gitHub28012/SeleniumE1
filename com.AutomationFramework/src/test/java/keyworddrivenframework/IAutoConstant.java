74a72e60e2f2d0919716994d13f51e9a532d648c
QSpiders <QSpiders@DESKTOP-U3SNJL3>
2024-03-05 20:27:14 +0530

    Keyword Driven Framework Updates

--- com.AutomationFramework/src/test/java/keyworddrivenframework/IAutoConstant.java
+++ com.AutomationFramework/src/test/java/keyworddrivenframework/IAutoConstant.java
@@ -4,4 +4,6 @@ public interface IAutoConstant {
 
 	String PROP_PATH="./src/test/resources/config.properties";
 	
+	String EXCEL_PATH="./src/test/resources/testData.xlsx";
+	
 }
--- com.AutomationFramework/src/test/java/keyworddrivenframework/ValidLoginTestCase.java
+++ com.AutomationFramework/src/test/java/keyworddrivenframework/ValidLoginTestCase.java
@@ -4,7 +4,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.Properties;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
