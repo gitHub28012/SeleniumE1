5cf7592e13bf25ed08275ab11684aba7385ef97d
QSpiders <QSpiders@DESKTOP-U3SNJL3>
2024-03-11 20:18:22 +0530

    TestNG Scripts

---
+++ com.AutomationFramework/src/test/java/learningTestNG/FirstTestNGClass.java
@@ -0,0 +1,26 @@
+package learningTestNG;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class FirstTestNGClass {
+  @Test(priority = 1)
+  public void login()
+  {
+	  Reporter.log("This is Login Method", true);
+  }
+  
+  @Test
+  public void addToCart()
+  {
+	  Reporter.log("This is Add To Cart Method", true);
+  }
+  
+  @Test
+  public void logout() 
+  {
+	  Reporter.log("This is Logout Method", true);
+  }
+}
