855773bb38c45cf9efa69b3968fee167cf5be682
QSpiders <QSpiders@DESKTOP-U3SNJL3>
2024-03-13 20:28:59 +0530

    testNG

---
+++ com.AutomationFramework/src/test/java/testNGExecution/Remo.java
@@ -0,0 +1,24 @@
+package testNGExecution;
+
+import java.time.Duration;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Remo {
+	@Test
+	public void remo() throws InterruptedException {
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.get("https://www.google.co.in/");
+		Thread.sleep(2000);
+
+		driver.switchTo().activeElement().sendKeys("Selenium", Keys.ENTER);
+		Thread.sleep(3000);
+		driver.quit();
+	}
+}
